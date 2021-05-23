0075543c5b31dd4d48042cb8cfbaa0132ca87faf
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-05-23 19:05:10 +0530

    Commit1

---
+++ fashioninsta/src/Components/App/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import HomePage from '../Home/HomePage';
+import './App.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
+}
+
+export default App;
