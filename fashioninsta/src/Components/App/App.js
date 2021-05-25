fc26fa973cc250ffdc186be3a5f7f322a08b768d
Rishabh tripathi <Rishabh.tripathi134@gmail.com>
2021-05-25 22:57:41 +0530

    Commit3

--- fashioninsta/src/Components/App/App.js
+++ fashioninsta/src/Components/App/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import HomePage from '../../pages/Home/HomePage';
+import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render(){
     return (
       <div>
-        <HomePage />
+        {/* <HomePage /> */}
+        <BrowserRouter>
+          <Switch>
+            <Route path= '/' component={HomePage} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
